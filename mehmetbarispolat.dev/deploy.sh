446ea3067bd06b60f8b1d6294f7f07888fb6ab14
Mehmet Baris POLAT <mehmetbarispolat@gmail.com>
2022-10-04 02:30:21 +0300

    Added deploy sh

---
+++ mehmetbarispolat.dev/deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'New Deployment for Production'
+git push -f git@github.com:mehmetbarispolat/mehmetbarispolat.dev.git master:gh-pages
+
+cd -
