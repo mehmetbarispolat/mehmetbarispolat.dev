3b35bf6b7963fef75569d56b65aeb72db089fb8c
Mehmet Baris POLAT <mehmetbarispolat@gmail.com>
2022-10-06 01:51:43 +0300

    added to --all field

--- mehmetbarispolat.dev/deploy.sh
+++ mehmetbarispolat.dev/deploy.sh
@@ -8,7 +8,7 @@ npm run build
 cd dist
 
 git config --global user.name "mehmetbarispolat" && git config --global user.email "mehmetbarispolat@gmail.com"
-git --work-tree add -A
+git --work-tree add --all
 git --work-tree commit -m 'New Deployment for Production'
 git push -f git@github.com:mehmetbarispolat/mehmetbarispolat.dev.git master:deploy-branch
 git checkout -f master
