da0aa2b6a1898cb3e580585284935522f7baefb7
Mehmet Baris POLAT <mehmetbarispolat@gmail.com>
2022-10-06 01:48:00 +0300

    adjusted deploy sh add work tree

--- mehmetbarispolat.dev/deploy.sh
+++ mehmetbarispolat.dev/deploy.sh
@@ -1,14 +1,17 @@
 #! /bin/sh
 
 set -e
+git checkout --orphan deploy-branch
 
 npm run build
 
 cd dist
 
 git config --global user.name "mehmetbarispolat" && git config --global user.email "mehmetbarispolat@gmail.com"
-git add -A
-git commit -m 'New Deployment for Production'
+git --work-tree add -A
+git --work-tree commit -m 'New Deployment for Production'
 git push -f git@github.com:mehmetbarispolat/mehmetbarispolat.dev.git master:deploy-branch
+git checkout -f master
+git branch -D deploy-branch
 
 cd -
