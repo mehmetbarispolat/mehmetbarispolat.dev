3c2c4a36dd8ba042d14f8eb259eceaaa08aced25
mehmetbarispolat <mehmetbarispolat@gmail.com>
2022-10-09 02:55:45 +0300

    deploy branch changed

--- mehmetbarispolat.dev/scripts/deploy.sh
+++ mehmetbarispolat.dev/scripts/deploy.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
 
 set -e
-git checkout --orphan deploy-branch
+git checkout --orphan gh-deploy
 
 npm run build
 
 git config --global user.name "mehmetbarispolat" && git config --global user.email "mehmetbarispolat@gmail.com"
 git --work-tree dist add --all
 git --work-tree dist commit -m 'New Deployment for Production'
-git push git@github.com:mehmetbarispolat/mehmetbarispolat.dev.git master:deploy-branch --force
+git push origin gh-deploy --force
 rm -r dist
 git checkout -f master
-git branch -D deploy-branch
 
 cd -
