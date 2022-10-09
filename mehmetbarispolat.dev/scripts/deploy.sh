38acd710ff4aae9683506e4f8034fe74dac64e12
mehmetbarispolat <mehmetbarispolat@gmail.com>
2022-10-09 03:07:58 +0300

    added git ssh

--- mehmetbarispolat.dev/scripts/deploy.sh
+++ mehmetbarispolat.dev/scripts/deploy.sh
@@ -8,7 +8,7 @@ npm run build
 git config --global user.name "mehmetbarispolat" && git config --global user.email "mehmetbarispolat@gmail.com"
 git --work-tree dist add --all
 git --work-tree dist commit -m 'New Deployment for Production'
-git push origin gh-deploy --force
+git push git@github.com:mehmetbarispolat/mehmetbarispolat.dev.git master:gh-deploy --force
 rm -r dist
 git checkout -f master
 
