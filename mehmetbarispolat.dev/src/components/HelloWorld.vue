def218679a5fa8344e1aefb135a52c2aa4e0ea28
mehmetbarispolat <mehmetbarispolat@gmail.com>
2023-07-28 00:34:55 +0300

    added dev container

--- mehmetbarispolat.dev/src/components/HelloWorld.vue
+++ mehmetbarispolat.dev/src/components/HelloWorld.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+    <h1>Hello, I'm Baris</h1>
   </div>
 </template>
 
@@ -22,18 +14,14 @@ h1 {
   top: -10px;
 }
 
-h3 {
-  font-size: 1.2rem;
-}
 
-.greetings h1,
-.greetings h3 {
+.greetings h1 {
   text-align: center;
 }
 
 @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
+
+  .greetings h1 {
     text-align: left;
   }
 }
